06a86ca965da5b13def103bd08e6d58b0e7bb6b2
mohamedis8890 <mohamed.is8890@gmail.com>
2019-01-18 18:09:02 +0200

    my version

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
-const calendarButton = document.querySelector(".btn-start");
+const calendarButton = document.querySelector('.btn-start');
 const calendarContainer = document.querySelector(".container");
 
 const calendarDays = 24;
 
-const openDoor = (path, event) => {
-    event.target.parentNode.style.backgroundImage = `url(${path})`;
-    event.target.style.opacity = "0";
-    event.target.style.backgroundColor = "#521751";
-}
+const openDoor = (imagePath, event) => {
+  event.target.parentNode.style.backgroundImage = `url(${imagePath})`;
+  event.target.style.opacity = "0";
+  event.target.style.backgroundColor = "#521751";
+};
 
 const createCalendar = () => {
-    for(let i = 0; i  < calendarDays; i++) {
-        const calendarDoor = document.createElement("div");
-        const calendarDoorText = document.createElement("div");
 
-        calendarDoor.classList.add("image");
-        calendarDoor.style.gridArea = "door" + (i + 1);
-        calendarContainer.appendChild(calendarDoor);
+  for (let i = 0; i < calendarDays; i++) {
+    const calendarDoor = document.createElement('div');
+    const calendarDoorText = document.createElement('div');
 
-        calendarDoorText.classList.add("text");
-        calendarDoorText.innerHTML = i + 1;
-        calendarDoor.appendChild(calendarDoorText);
+    calendarDoor.classList.add('image');
+    calendarDoor.style.gridArea = 'door' + (i + 1);
+    calendarContainer.appendChild(calendarDoor);
 
-        courseNumber = i + 1;
-        let coursePath = `./courses/course-${courseNumber}.jpg`;
+    calendarDoorText.classList.add('text');
+    calendarDoorText.innerHTML = i + 1;
+    calendarDoor.appendChild(calendarDoorText);
 
-        calendarDoorText.addEventListener("click", openDoor.bind(null,  coursePath));
-    }
-}
+    let imagePath = `./courses/course-${i + 1}.jpg`;
+    calendarDoorText.addEventListener('click', openDoor.bind(null, imagePath));
+  }
+};
 
-calendarButton.addEventListener("click", createCalendar);
-
+calendarButton.addEventListener('click', createCalendar);
